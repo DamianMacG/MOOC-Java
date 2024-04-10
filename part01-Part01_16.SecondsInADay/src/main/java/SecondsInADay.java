9916ce53d44ddc450aef90b62e4696a96086318f
damiang <damiangildea12@gmail.com>
2024-04-10 11:51:35 +0100

    Task 16 completed

--- part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -6,7 +6,10 @@ public class SecondsInADay {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("How many days would you like to convert to seconds?");
+        int number = Integer.valueOf(scanner.nextLine());
+        int toSeconds = 60 * 60 * 24 * number;
+        System.out.println(toSeconds);
 
     }
 }
