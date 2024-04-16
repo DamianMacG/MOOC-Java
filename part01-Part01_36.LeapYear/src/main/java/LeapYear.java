70e4c840a168c1e2732efb348fffcbc4187a36bc
damiang <damiangildea12@gmail.com>
2024-04-16 13:02:08 +0100

    Task 36 complete

--- part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -6,5 +6,17 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Give a year:");
+        String year = scan.nextLine();
+        if (Integer.parseInt(year) % 400 == 0 && Integer.parseInt(year) % 100 == 0) {
+            System.out.println("The year is a leap year.");
+        } else if (Integer.parseInt(year) % 400 != 0 && Integer.parseInt(year) % 100 == 0) {
+            System.out.println("The year is not a leap year.");
+        } else if (Integer.parseInt(year) % 4 == 0) {
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
+
     }
 }
