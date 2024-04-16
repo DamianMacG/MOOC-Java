b370884a7ecee2c4af2bf149c3ff734a82955aa2
damiang <damiangildea12@gmail.com>
2024-04-16 12:53:00 +0100

    Task 35 complete

--- part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
+++ part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
@@ -6,5 +6,14 @@ public class CheckingTheAge {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("How old are you?");
+        String first = scan.nextLine();
+
+        if (Integer.parseInt(first) <= 120 && Integer.parseInt(first) >= 0) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Impossible!");
+        }
+
     }
 }
