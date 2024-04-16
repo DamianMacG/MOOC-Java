ead977c2146765669fd4c2334f16eebd310a5369
damiang <damiangildea12@gmail.com>
2024-04-16 10:39:00 +0100

    Task 21 complete

--- part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
+++ part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class BooleanInput {
 
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
