bbf6b7614f3bd3cab0551ad3be9cc590aba8f4bc
damiang <damiangildea12@gmail.com>
2024-04-22 13:09:40 +0100

    Part 2 task 1 completed

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 public class Squared {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+
+        int input = Integer.valueOf(reader.nextLine());
+
+        // Identifying the operation and declaring a variable for the result
+        int product = input * input;
+
+        // Printing the result of the operation
+        System.out.println(product);
 
     }
 }
