8fbf09dbef1799ced3af61573d884708e2d84ef2
damiang <damiangildea12@gmail.com>
2024-04-22 14:19:49 +0100

    Part 2 task 7 completed

--- part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -6,5 +6,17 @@ public class OnlyPositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Give a number:");
+            int command = Integer.valueOf(scanner.nextLine());
+            if (command == 0) {
+                break;
+            } else if (command > 0) {
+                System.out.println(command * command);
+            } else if (command < 0) {
+                System.out.println("Unsuitable number");
+            }
+        }
+
     }
 }
