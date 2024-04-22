89befe03405924eba31932907e7638c50d1f5b4f
damiang <damiangildea12@gmail.com>
2024-04-22 14:30:48 +0100

    Part 2 task 9 completed

--- part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -6,5 +6,20 @@ public class NumberOfNegativeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            } else if (number < 0) {
+                sum += 1;
+            }
+
+        }
+        System.out.println("Number of negative numbers: " + sum);
+
     }
 }
