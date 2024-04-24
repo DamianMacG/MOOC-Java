92b9e6a2f3e43cdbb8b4d1e0247939a2bbe6c36a
damiang <damiangildea12@gmail.com>
2024-04-24 13:43:35 +0100

    Part 2 task 13 completed

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,28 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int positiveCount = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            } else {
+                if (number > 0) {
+                    sum += number;
+                    positiveCount++;
+                }
+            }
+        }
+
+        if (positiveCount == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            double average = (double) sum / positiveCount;
+            System.out.println("Average of the positive numbers: " + average);
+        }
     }
 }
