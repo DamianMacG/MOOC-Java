1552a0ebd4f196dc68f345e5074ad331c4fdb0b2
damiang <damiangildea12@gmail.com>
2024-04-25 13:47:50 +0100

    Part 2 task 19 completed

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,15 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Give a number?");
+        int number1 = Integer.valueOf(scanner.nextLine());
+
+        int sum = 1;
+
+        for (int i = 1; i <= number1; i++) {
+            sum *= i;
+        }
+        System.out.println("Factorial: " + sum);
+
     }
 }
