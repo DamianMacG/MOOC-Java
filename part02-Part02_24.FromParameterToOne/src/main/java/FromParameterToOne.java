5d2fd2693314cc6b299163e31311f98311fa7423
damiang <damiangildea12@gmail.com>
2024-04-26 16:28:33 +0100

    Part 2 task 24 completed

--- part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
+++ part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -1,10 +1,16 @@
 
-
 public class FromParameterToOne {
 
     public static void main(String[] args) {
+        printFromNumberToOne(5);
+    }
 
-
+    public static void printFromNumberToOne(int number) {
+        int end = 1;
+        while (end <= number) {
+            System.out.println(number);
+            number--;
+        }
     }
 
 }
