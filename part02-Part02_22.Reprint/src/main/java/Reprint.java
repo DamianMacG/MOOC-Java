2f1fdc14c2b149b09185db9d13bb3429306ba09d
damiang <damiangildea12@gmail.com>
2024-04-26 16:08:52 +0100

    Part 2 task 22 completed

--- part02-Part02_22.Reprint/src/main/java/Reprint.java
+++ part02-Part02_22.Reprint/src/main/java/Reprint.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 public class Reprint {
 
     public static void main(String[] args) {
-        // ask the user for how many times should the text be printed
-        // then call the printText-method multiple times with a while-loop
-        
         Scanner scanner = new Scanner(System.in);
+        System.out.print("How many times?");
+        int num = Integer.valueOf(scanner.nextLine());
+        int sum = 0;
+
+        while (sum <= num) {
+
+            printText();
+            sum++;
+        }
+
     }
-    
+
     // Don't change the next line that defines the method!
     // (We aren't giving a method a parameter yet)
     public static void printText() {
-        // write some code here
+        System.out.println("In a hole in the ground there lived a method");
     }
 }
