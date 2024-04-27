ef2d1808886c385b9bd8f9dfaf1dabb293651b2c
damiang <damiangildea12@gmail.com>
2024-04-27 09:40:19 +0100

    Part 2 task 33 completed

--- part02-Part02_33.StarSign/src/main/java/StarSign.java
+++ part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -3,13 +3,12 @@ public class StarSign {
 
     public static void main(String[] args) {
 
-        
-        //The tests are not checking the main, so you can modify it freely.
-        //NB: If the tests don't seem to pass, you should try the methods here
-        //in the main to make sure they print the correct shapes!
-        
+        // The tests are not checking the main, so you can modify it freely.
+        // NB: If the tests don't seem to pass, you should try the methods here
+        // in the main to make sure they print the correct shapes!
+
         printStars(3);
-        System.out.println("\n---");  // printing --- between the shapes
+        System.out.println("\n---"); // printing --- between the shapes
         printSquare(4);
         System.out.println("\n---");
         printRectangle(5, 6);
@@ -19,18 +18,35 @@ public class StarSign {
     }
 
     public static void printStars(int number) {
-        // first part of the exercise
+        int start = 0;
+        while (number > start) {
+            System.out.print("*");
+            start++;
+        }
+        System.out.println();
     }
 
     public static void printSquare(int size) {
-        // second part of the exercise
+        int start = 0;
+        while (size > start) {
+            printStars(size);
+            start++;
+        }
     }
 
     public static void printRectangle(int width, int height) {
-        // third part of the exercise
+        int start = 0;
+        while (start < height) {
+            printStars(width);
+            start++;
+        }
     }
 
     public static void printTriangle(int size) {
-        // fourth part of the exercise
+        int start = 1;
+        while (start <= size) {
+            printStars(start);
+            start++;
+        }
     }
 }
