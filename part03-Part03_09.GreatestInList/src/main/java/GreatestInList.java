34a7d07b4c009aafdef2c3e3169501c9fed52f9e
damiang <damiangildea12@gmail.com>
2024-04-29 10:44:47 +0100

    Part 3 task 9 completed

--- part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
+++ part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class GreatestInList {
@@ -16,9 +17,7 @@ public class GreatestInList {
 
             list.add(input);
         }
-        
-        System.out.println("");
 
-        // implement finding the greatest number in the list here
+        System.out.println("The greatest number: " + Collections.max(list));
     }
 }
