679c58402402c8c7b1deaebde07d4fb3f36c4282
damiang <damiangildea12@gmail.com>
2024-05-01 09:15:30 +0100

    Part 3 task 26 completed

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -2,10 +2,21 @@
 import java.util.Scanner;
 
 public class LineByLine {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String input = scanner.nextLine();
 
+            if (input.length() == 0) {
+                break;
+            }
+
+            String[] parts = input.split(" ");
+
+            for (int i = 0; i < parts.length; i++) {
+                System.out.println(parts[i]);
+            }
+        }
     }
 }
