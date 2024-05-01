bf5c8790de00f51f5a89f9c45c803e20ba9a2c1c
damiang <damiangildea12@gmail.com>
2024-05-01 10:05:11 +0100

    Part 3 task 31 completed

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -6,6 +6,29 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int max = 0;
+        String name = "";
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.isEmpty()) {
+                break;
+            }
+
+            String[] parts = input.split(",");
+
+            if (parts.length > 1) {
+                int age = Integer.valueOf(parts[1].trim());
+                String name1 = parts[0].trim();
+                if (age > max) {
+                    max = age;
+                    name = name1;
+                }
+
+            }
+            System.out.println(name);
+        }
 
     }
 }
