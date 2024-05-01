2bd7574c338140c8ef8125c0dedf20997b048bf2
damiang <damiangildea12@gmail.com>
2024-05-01 10:00:44 +0100

    Part 3 task 30 completed

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -6,6 +6,25 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int max = 0;
 
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.isEmpty()) {
+                break;
+            }
+
+            String[] parts = input.split(",");
+
+            if (parts.length > 1) {
+                int age = Integer.valueOf(parts[1].trim());
+                if (age > max) {
+                    max = age;
+                }
+
+            }
+            System.out.println(max);
+        }
     }
 }
