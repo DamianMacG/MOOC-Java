f67ceead48cb0d923358456baef5cbd14008d7e9
damiang <damiangildea12@gmail.com>
2024-05-01 10:16:01 +0100

    Part 3 task 32 completed

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -1,5 +1,3 @@
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PersonalDetails {
@@ -7,6 +5,35 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int count = 0; // Track the count of valid inputs
+        String name = "";
 
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.isEmpty()) {
+                break;
+            }
+
+            String[] parts = input.split(",");
+
+            if (parts.length > 1) {
+                int age = Integer.valueOf(parts[1].trim());
+                sum = sum + age;
+                count++; // Increment the count of valid inputs
+                String name1 = parts[0].trim();
+                if (name1.length() > name.length()) {
+                    name = name1;
+                }
+
+            }
+        }
+
+        // Calculate average after all input is processed
+        double average = (double) sum / count;
+
+        System.out.println("Longest name: " + name);
+        System.out.println("Average of the birth years: " + average);
     }
 }
