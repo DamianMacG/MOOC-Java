ebfebea5650dea34586d3883354c701e1ed3f935
damiang <damiangildea12@gmail.com>
2024-05-01 08:52:13 +0100

    Part 3 task 23 completed

--- part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
+++ part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
@@ -6,7 +6,10 @@ public class PrintThrice {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.print("Give a word: ");
+        String name = scanner.nextLine();
+
+        System.out.print(name + name + name);
 
     }
 }
