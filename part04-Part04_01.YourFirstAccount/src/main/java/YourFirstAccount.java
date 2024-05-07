1d2d26040e0703ade66d5addd1252e6634a72aac
damiang <damiangildea12@gmail.com>
2024-05-07 09:25:53 +0100

    Part 4 tasks 1 and 2 completed

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -2,7 +2,10 @@
 public class YourFirstAccount {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // Write your program here
+        Account DamianAccount = new Account("Damian's account", 100.00);
+
+        DamianAccount.deposit(20);
+
+        System.out.println(DamianAccount);
     }
 }
--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -2,7 +2,13 @@
 public class YourFirstBankTransfer {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // write your program here
+        Account Matthew = new Account("Matthews account", 1000.0);
+        Account Damian = new Account("My account", 0.0);
+
+        Matthew.withdrawal(100.0);
+        Damian.deposit(100.0);
+
+        System.out.println(Matthew);
+        System.out.println(Damian);
     }
 }
