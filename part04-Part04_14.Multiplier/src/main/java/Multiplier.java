ab2c53eeb342e256ea6a9c89532fc5eeecb2369f
damiang <damiangildea12@gmail.com>
2024-05-14 12:56:24 +0100

    Part 4 task 14 completed

---
+++ part04-Part04_14.Multiplier/src/main/java/Multiplier.java
@@ -0,0 +1,13 @@
+public class Multiplier {
+
+    private int input;
+
+    public Multiplier(int number) {
+        input = number;
+    }
+
+    public int multiply(int number) {
+        return input * number;
+    }
+
+}
