e47d29e3aa279c5a62ecbe70c6b10c373e24b255
damiang <damiangildea12@gmail.com>
2024-05-21 11:00:14 +0100

    Part 4 task 16 second part done

--- part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
+++ part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
@@ -8,4 +8,12 @@ public class PaymentCard {
     public String toString() {
         return "The card has a balance of " + balance + " euros.";
     }
+
+    public void eatAffordably() {
+        balance -= 2.6;
+    }
+
+    public void eatHeartily() {
+        balance -= 4.6;
+    }
 }
