bf4add064752a3350b04ea7ee0fa4276a650e66a
damiang <damiangildea12@gmail.com>
2024-05-21 10:58:28 +0100

    Part 4 task 16 basic setup

---
+++ part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
@@ -0,0 +1,11 @@
+public class PaymentCard {
+    private double balance;
+
+    public PaymentCard(double openingBalance) {
+        balance += openingBalance;
+    }
+
+    public String toString() {
+        return "The card has a balance of " + balance + " euros.";
+    }
+}
