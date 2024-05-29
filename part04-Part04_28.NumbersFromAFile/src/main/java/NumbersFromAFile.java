e3472883d98e8ed6f6305a8018d7b805bc9c3f45
damiang <damiangildea12@gmail.com>
2024-05-29 12:39:45 +0100

    Part 4 task 28 completed

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -1,5 +1,9 @@
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NumbersFromAFile {
@@ -13,6 +17,25 @@ public class NumbersFromAFile {
         int lowerBound = Integer.valueOf(scanner.nextLine());
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
+
+        ArrayList<String> list = new ArrayList<>();
+
+        Path filename = Paths.get(file);
+
+        try {
+            list.addAll(Files.readAllLines(filename));
+        } catch (IOException e) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
+        int count = 0;
+        for (int i = 0; i < list.size(); i++) {
+            int num = Integer.valueOf(list.get(i));
+            if (num >= lowerBound && num <= upperBound) {
+                count++;
+            }
+
+        }
+        System.out.println("Numbers: " + count);
 
     }
 
