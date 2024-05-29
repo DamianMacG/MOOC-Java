aaa3d3442452f122393a9965304370130f0c4327
damiang <damiangildea12@gmail.com>
2024-05-29 08:59:33 +0100

    Part 4 task 21 completed

--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -4,7 +4,19 @@ import java.util.Scanner;
 public class NumberOfStrings {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+        int index = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+
+            String word = scanner.nextLine();
+            if (!word.equals("end")) {
+                index = index + 1;
+            } else {
+                break;
+            }
+
+        }
+        System.out.print(index);
     }
 }
