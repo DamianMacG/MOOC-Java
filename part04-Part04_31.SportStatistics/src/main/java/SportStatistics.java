af5a4d3bd8f285e38d79a2b2e70a7ba233b5e18b
damiang <damiangildea12@gmail.com>
2024-05-29 13:58:59 +0100

    Part 4 task 31 completed

--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -1,13 +1,47 @@
-
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SportStatistics {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("File: ");
+        String file = scanner.nextLine();
+        System.out.print("Team: ");
+        String team = scanner.nextLine();
 
+        int count = 0;
+        int wins = 0;
+        int losses = 0;
+        Path filename = Paths.get(file);
+
+        try (Scanner fileReader = new Scanner(filename)) {
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                String[] parts = line.split(",");
+
+                String name1 = parts[0];
+                String name2 = parts[1];
+                int name1Points = Integer.valueOf(parts[2]);
+                int name2Points = Integer.valueOf(parts[3]);
+
+                if (name1.equals(team) || name2.equals(team)) {
+                    count++;
+                    if ((name1.equals(team) && name1Points > name2Points) ||
+                            (name2.equals(team) && name2Points > name1Points)) {
+                        wins++;
+                    } else {
+                        losses++;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Could not read the specified file.");
+        }
+
+        System.out.println("Games: " + count);
+        System.out.println("Wins: " + wins);
+        System.out.println("Losses: " + losses);
     }
-
 }
