bac38dcd854e06b9167cd7c46d873d89c656d971
damiang <damiangildea12@gmail.com>
2024-05-29 13:18:31 +0100

    Part 4 task 29 completed

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -1,4 +1,5 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -6,6 +7,29 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Name of the file:");
+        String file = scanner.nextLine();
+
+        try (Scanner scanner1 = new Scanner(Paths.get(file))) {
+
+            while (scanner1.hasNextLine()) {
+                String line = scanner1.nextLine();
+
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+                if (age == 1) {
+
+                    System.out.println(name + ", age: " + age + " year");
+                    System.out.println("Age: " + age);
+                } else {
+                    System.out.println(name + ", age: " + age + " years");
+                    System.out.println("Age: " + age);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
 
     }
 }
