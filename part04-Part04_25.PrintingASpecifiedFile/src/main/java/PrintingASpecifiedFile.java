8362b8c0ac1e81388ca28b3625fbc82805a49425
damiang <damiangildea12@gmail.com>
2024-05-29 09:47:36 +0100

    Part 4 task 25 completed

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -1,11 +1,27 @@
-
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.IOException;
 
 public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Which file should have its contents printed?");
+        String fileName = scanner.nextLine();
 
+        ArrayList<String> lines = new ArrayList<>();
+
+        try (Scanner fileScanner = new Scanner(Paths.get(fileName))) {
+            while (fileScanner.hasNextLine()) {
+                lines.add(fileScanner.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        for (int i = 0; i < lines.size(); i++) {
+            System.out.println(lines.get(i));
+        }
     }
 }
