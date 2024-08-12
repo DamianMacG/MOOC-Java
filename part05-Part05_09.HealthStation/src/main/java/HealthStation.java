1c359999ef66d5108fb3168f331338cba8f0f920
damiang <damiangildea12@gmail.com>
2024-08-12 14:06:32 +0100

    Part 5 Task 9 completed

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,22 @@
 
 public class HealthStation {
-
+    private int count = 0;
 
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
-        return -1;
+        count++;
+        return person.getWeight();
+
+    }
+
+    public void feed(Person person) {
+        int newWeight = person.getWeight();
+        person.setWeight(newWeight + 1);
+    }
+
+    public int weighings() {
+
+        return count;
     }
 
 }
