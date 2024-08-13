b7e9bce8600c8157e37c8f87bfb5833cee5d2d32
damiang <damiangildea12@gmail.com>
2024-08-13 08:47:30 +0100

    Part 5 Task 11 (2 of 3) completed

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -11,4 +11,22 @@ public class Apartment {
         this.princePerSquare = pricePerSquare;
     }
 
+    public boolean largerThan(Apartment compared) {
+
+        if (compared.squares > squares) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public int priceDifference(Apartment compared) {
+        if (compared.princePerSquare * compared.squares > princePerSquare * squares) {
+            return compared.princePerSquare * compared.squares - princePerSquare * squares;
+        } else {
+            return princePerSquare * squares - compared.princePerSquare * compared.squares;
+        }
+
+    }
+
 }
