729f821aaa8365ea13540aa54ebdf65ce9b28943
damiang <damiangildea12@gmail.com>
2024-09-02 11:30:22 +0100

    part 5 task 16 completed

--- part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
+++ part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
@@ -26,11 +26,37 @@ public class SimpleDate {
         }
 
         if (this.year == compared.year && this.month == compared.month &&
-                 this.day < compared.day) {
+                this.day < compared.day) {
             return true;
         }
 
         return false;
+    }
+
+    public void advance() {
+        advance(1);
+    }
+
+    public void advance(int howManyDays) {
+        this.day += howManyDays;
+        while (this.day > 30) {
+            this.day -= 30;
+            this.month++;
+            if (this.month > 12) {
+                this.month = 1;
+                this.year++;
+            }
+        }
+    }
+
+    public SimpleDate afterNumberOfDays(int days) {
+        // Create a new date object based on the current date
+        SimpleDate newDate = new SimpleDate(this.day, this.month, this.year);
+
+        // Advance the new date by the specified number of days
+        newDate.advance(days);
+
+        return newDate;
     }
 
 }
