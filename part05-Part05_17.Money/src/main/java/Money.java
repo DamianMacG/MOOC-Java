383d22691a04ac0d3ba94c653e5d77fee1d04ab4
damiang <damiangildea12@gmail.com>
2024-09-02 12:47:25 +0100

    part 5 task 17 completed

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -32,4 +32,43 @@ public class Money {
         return this.euros + "." + zero + this.cents + "e";
     }
 
+    public Money plus(Money addition) {
+        int totalEuros = addition.euros;
+        int totalCents = addition.cents;
+        totalEuros += this.euros;
+        totalCents += this.cents;
+
+        // return the new Money object
+        return new Money(totalEuros, totalCents);
+    }
+
+    public boolean lessThan(Money compared) {
+        // Convert both money objects to total cents
+        int thisTotalCents = this.euros * 100 + this.cents;
+        int comparedTotalCents = compared.euros * 100 + compared.cents;
+
+        // Compare the total cents
+        return thisTotalCents < comparedTotalCents;
+    }
+
+    public Money minus(Money decreaser) {
+        // Convert current Money and decreaser Money to total cents
+        int thisTotalCents = this.euros * 100 + this.cents;
+        int decreaserTotalCents = decreaser.euros * 100 + decreaser.cents;
+
+        // Calculate the difference in total cents
+        int differenceTotalCents = thisTotalCents - decreaserTotalCents;
+
+        // If difference is negative, set to zero
+        if (differenceTotalCents < 0) {
+            differenceTotalCents = 0;
+        }
+
+        // Convert back to euros and cents
+        int resultEuros = differenceTotalCents / 100;
+        int resultCents = differenceTotalCents % 100;
+
+        // Create and return a new Money object with the result
+        return new Money(resultEuros, resultCents);
+    }
 }
